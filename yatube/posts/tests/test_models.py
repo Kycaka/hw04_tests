290624f77b285ff21b116d21bb4f3a5f52c97f12
Renat Emaletdinov <spamhatertom@gmail.com>
2022-11-06 13:08:30 +0300

    first try

---
+++ yatube/posts/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ..models import Group, Post
+
+User = get_user_model()
+
+SHORT_GROUP_TEXT = 15
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у моделей корректно работает __str__."""
+        post = PostModelTest.post
+        group = PostModelTest.group
+        expected_object_names = {
+            post: post.text[:SHORT_GROUP_TEXT],
+            group: group.title,
+        }
+        for field, expected in expected_object_names.items():
+            with self.subTest(field=field):
+                self.assertEqual(expected, str(field))
